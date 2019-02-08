447579dfc96105f0627c7131ae0ffaa6688546f0
Juliann Beals <juliannbeals@gmail.com>
2019-02-08 11:07:40 +0100

    5.3 tryingt o fix signout bug

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
     :sign_in => 'login',
     :sign_out => 'logout',
     :controller=>'devise/sessions', :action=>'destroy'}
+
   resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+  
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
