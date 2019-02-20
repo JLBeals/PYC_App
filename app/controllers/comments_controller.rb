524786b40cd03fe806df110870dcfe59f589e809
Juliann Beals <juliannbeals@gmail.com>
2019-02-20 11:54:58 +0100

    5.8 still fighting those bugs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,4 +10,11 @@ class CommentsController < ApplicationController
   def destroy
   end
 
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :body, :rating)
+  end
+
 end
