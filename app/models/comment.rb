5af0fdc92a28df45aeeda4fa6af8aba1fa7e948b
Juliann Beals <juliannbeals@gmail.com>
2019-02-27 10:52:03 +0100

    5.11 added a numericality validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
-  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
   scope :rating_desc, -> {order(rating: :desc)}
   scope :rating_asc, -> {order(rating: :asc)}
