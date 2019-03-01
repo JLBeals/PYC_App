4ac985e0c42865fe3bb2eb8ce73dea12b34328ce
Juliann Beals <juliannbeals@gmail.com>
2019-03-01 08:06:06 +0100

    5.11 added a welcome email for new users and did styling to the page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
         to: 'juliannbeals@gmail.com',
         subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  @appname = "Prague Yoga Collective"
+  mail(to: user.email,
+    subject: "Welcome to #{@appname}!")
+  end
+
 end
