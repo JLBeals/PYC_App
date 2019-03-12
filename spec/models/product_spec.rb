58423a14a373aef8b60653de2b94979fefed886c
Juliann Beals <juliannbeals@gmail.com>
2019-03-12 18:46:37 +0100

    6.4 deleted db test sqlite3 file to fix bug in tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,12 +4,12 @@ describe Product do
   context "when the product has comments" do
       let(:product) { Product.create!(name: "yoga pass") }
       # let(:user) {User.create!(email: "test4@gmail.com", password: "123456789") }
-      @user = FactoryBot.create(:user)
+      let(:user) { FactoryBot.create(:user) }
 
     before do
-      product.comments.create!(rating: 1, user: @user, body: "Bleh")
-      product.comments.create!(rating: 3, user: @user, body: "Okay")
-      product.comments.create!(rating: 5, user: @user, body: "Love")
+      product.comments.create!(rating: 1, user: user, body: "Bleh")
+      product.comments.create!(rating: 3, user: user, body: "Okay")
+      product.comments.create!(rating: 5, user: user, body: "Love")
     end
 
     it "returns the average rating of all comments" do
