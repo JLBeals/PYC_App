526d8cb4b2ffe969f4ae23c0f3993e4743703e17
Juliann Beals <juliannbeals@gmail.com>
2019-04-01 20:26:59 +0200

    6.13 added ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://pragueyogacollective.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://pragueyogacollective.herokuapp.com', 'http://pragueyogacollective.herokuapp.com']
+
 end
