415f434bb9e22b283093a8d7f174bef69a338825
Juliann Beals <juliannbeals@gmail.com>
2019-04-02 21:36:09 +0200

    6.14 Added ActiveJob and Advanced ActionCable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,7 @@ class Comment < ApplicationRecord
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 
   scope :rating_desc, -> {order(rating: :desc)}
   scope :rating_asc, -> {order(rating: :asc)}
